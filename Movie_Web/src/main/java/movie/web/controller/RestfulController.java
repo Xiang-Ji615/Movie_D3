704e3273139a4bbaf0de746f3569b00e8a2660df
xiang <xiang@DESKTOP-K0QCT21.home>
2018-04-15 21:59:56 +1000

    Add init DB REST service

--- Movie_Web/src/main/java/movie/web/controller/RestfulController.java
+++ Movie_Web/src/main/java/movie/web/controller/RestfulController.java
@@ -1,17 +1,24 @@
 package main.java.movie.web.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import main.java.movie.bo.bo.IMovieBo;
+import main.java.movie.common.file.lister.IFileLister;
+import main.java.movie.common.file.model.Movie;
+import main.java.movie.common.file.reader.IFileReader;
+import main.java.movie.dao.dao.IMovieDao;
 import main.java.movie.dao.model.MovieGrossType;
 
 @RestController
@@ -21,6 +28,17 @@ public class RestfulController {
 
 	@Autowired
 	IMovieBo movieBo;
+	
+	@Autowired
+	@Qualifier("csvFileLister")
+	IFileLister<File> fileLister;
+	
+	@Autowired
+	@Qualifier("csvFileReaderImp")
+	IFileReader<File, List<Movie>> csvMovieReader;
+	
+	@Autowired
+	IMovieDao movieDao;
 	
 	@RequestMapping(value="MovieGrossType", method=RequestMethod.GET)
 	public Object getMovieGrossType(){
@@ -36,5 +54,15 @@ public class RestfulController {
 		}
 		ret.put("children", data);
 		return ret;
+	}
+	
+	@RequestMapping(value="InitData", method=RequestMethod.GET)
+	public Object initData() throws IOException {
+		List<File> files = fileLister.listFilesInFolder("/Users/wenshi/Documents/Movie");
+		for(File file : files) {
+			List<Movie> movies = csvMovieReader.readFile(file);
+			movieDao.saveMovies(movies);
+		}
+		return true;
 	}
 }
